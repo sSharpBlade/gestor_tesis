be654e17bf7d9289483dd8dd5a5e8cbb0db84d35
SharpBlade <yepezedwin760@gmail.com>
2024-05-25 20:33:29 -0500

    Componente de Lista de Estudiantes

--- src/home/ListStudents.tsx
+++ src/home/ListStudents.tsx
@@ -1,5 +1,103 @@
-function ListStudent() {
-  return <div>ListStudent</div>;
+import React from "react";
+import { Table, Tag, Avatar, Button } from "antd";
+import type { TableColumnsType } from "antd";
+import { DataType } from "./types";
+import EditFilled from "@ant-design/icons/lib/icons/EditFilled";
+import { datos } from "./data";
+
+interface ListStudentsProps {
+  data: DataType[];
 }
 
-export default ListStudent;
+const generateFilters = (data: DataType[], key: keyof DataType) => {
+  const uniqueValues = Array.from(new Set(data.map((item) => item[key])));
+  return uniqueValues.map((value) => ({
+    text: String(value),
+    value: String(value),
+  }));
+};
+
+const percentageFilters = [
+  { text: "<50", value: "<50" },
+  { text: "50", value: "50" },
+  { text: ">50", value: ">50" },
+];
+
+const columns: TableColumnsType<DataType> = [
+  {
+    width: 20,
+    render: (_, { name: firstname }) => (
+      <Avatar size={24} src={`https://ui-avatars.com/api/?name=${firstname}`} />
+    ),
+  },
+  {
+    title: "Estudiante",
+    dataIndex: "name",
+    width: 150,
+  },
+  {
+    title: "Carrera",
+    dataIndex: "career",
+    filters: generateFilters(datos, "career"),
+    onFilter: (value, record) => record.career === value,
+    width: 150,
+  },
+  {
+    title: "Estado",
+    dataIndex: "state",
+    filters: generateFilters(datos, "state"),
+    onFilter: (value, record) => record.state === value,
+    render: (state) => {
+      const color =
+        state === "Proceso" ? "blue" : state === "Retirado" ? "red" : "green";
+      return (
+        <Tag color={color} key={state}>
+          {state}
+        </Tag>
+      );
+    },
+    width: 70,
+  },
+  {
+    title: "Porcentaje",
+    dataIndex: "percentage",
+    width: 70,
+    filters: percentageFilters,
+    onFilter: (value, record) => {
+      if (value === "<50") return record.percentage < 50;
+      if (value === "50") return record.percentage === 50;
+      if (value === ">50") return record.percentage > 50;
+      return false;
+    },
+  },
+  {
+    width: 50,
+    title: "Opciones",
+    render: ({ id }) => (
+      <Button
+        className="border-0 bg-transparent"
+        onClick={() => {
+          console.log(id);
+        }}
+      >
+        <EditFilled />
+      </Button>
+    ),
+  },
+];
+
+const ListStudents: React.FC<ListStudentsProps> = ({ data }) => {
+  return (
+    <Table
+      columns={columns}
+      dataSource={data.map((item) => ({ ...item, key: item.id }))}
+      pagination={{
+        pageSize: 10,
+        hideOnSinglePage: true,
+      }}
+      scroll={{ y: 650 }}
+    />
+  );
+};
+
+export default ListStudents;
