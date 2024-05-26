180998347e09ae1bc38e5d63d738ed9ea3bc95ab
SharpBlade <yepezedwin760@gmail.com>
2024-05-25 20:34:23 -0500

    Componente Menu

---
+++ src/home/Menu.tsx
@@ -0,0 +1,12 @@
+import IdcardFilled from "@ant-design/icons/lib/icons/IdcardFilled";
+import { Button } from "antd";
+
+function Menu() {
+  return (
+    <Button className="">
+      <IdcardFilled />
+    </Button>
+  );
+}
+
+export default Menu;
