3cb3259a089723f100ba9c6d91d02fcf69937709
TicheKiwar <kiwartiche@gmail.com>
2024-05-27 15:07:53 -0500

    Implements modal on menu.tsx

--- src/components/Student/StudentForm.tsx
+++ src/components/Student/StudentForm.tsx
@@ -3,134 +3,132 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './StudentForm.module.css';
-import { request } from './Student.request';
 
 interface StudentFormProps {
-    handleClose: () => void;
-    Teacher:number;
+  onSubmit: (data: any) => Promise<void>;
+  handleClose: () => void;
+  TeacherID:number
 }
 
 interface FormData {
-    cedula: string;
-    firstname: string;
-    lastname: string;
-    idCareer: string;
-    issue: string;
-    approvalDate: string;
+  cedula: string;
+  firstname: string;
+  lastname: string;
+  idCareer: string;
+  issue: string;
+  approvalDate: string;
 }
 
-const StudentForm: React.FC<StudentFormProps> = ({Teacher, handleClose }) => {
-    const { register, formState: { errors }, handleSubmit, setValue } = useForm<FormData>();
+const StudentForm: React.FC<StudentFormProps> = ({TeacherID, onSubmit, handleClose }) => {
+  const { register, formState: { errors }, handleSubmit, setValue } = useForm<FormData>();
 
-    const handleFormSubmit: SubmitHandler<FormData> = async (data) => {
-        const formattedData = {
-            student: {
-                cedula: data.cedula,
-                firstname: data.firstname,
-                lastname: data.lastname,
-                idCareer: parseInt(data.idCareer), // Convertir a número entero
-                idTeacher: Teacher // Asignar un valor predeterminado para idTeacher
-            },
-            thesis: {
-                issue: data.issue,
-                approvalDate: data.approvalDate
-            }
-        };
-        await request(formattedData);
-        handleClose(); // Cerrar el modal después de enviar el formulario
+  const handleFormSubmit: SubmitHandler<FormData> = async (data) => {
+    const formattedData = {
+      student: {
+        cedula: data.cedula,
+        firstname: data.firstname,
+        lastname: data.lastname,
+        idCareer: parseInt(data.idCareer), // Convertir a número entero
+        idTeacher: TeacherID // Asignar un valor predeterminado para idTeacher
+      },
+      thesis: {
+        issue: data.issue,
+        approvalDate: data.approvalDate
+      }
     };
+    await onSubmit(formattedData);
+    handleClose(); // Cerrar el modal después de enviar el formulario
+  };
 
-    const handleNumericInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const newValue = e.target.value;
-        if (/^\d*$/.test(newValue)) {
-            setValue(e.target.name as keyof FormData, newValue);
-        } else {
-            setValue(e.target.name as keyof FormData, newValue.slice(0, -1));
-        }
-        if (newValue.length === 11) {
-            setValue(e.target.name as keyof FormData, newValue.slice(0, -1));
-        }
-    };
+  const handleNumericInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = e.target.value;
+    if (/^\d*$/.test(newValue)) {
+      setValue(e.target.name as keyof FormData, newValue);
+    } else {
+      setValue(e.target.name as keyof FormData, newValue.slice(0, -1));
+    }
+    if (newValue.length === 11) {
+      setValue(e.target.name as keyof FormData, newValue.slice(0, -1));
+    }
+  };
 
-    useEffect(() => {
-        if (errors.cedula) {
-            if (errors.cedula.type === 'required') {
-                toast.error('Cedula es un campo requerido');
-            } else if (errors.cedula.type === 'minLength') {
-                toast.error('Cedula requiere una longitud mínima de 10 caracteres');
-            }
-        }
-    }, [errors.cedula]);
+  useEffect(() => {
+    if (errors.cedula) {
+      if (errors.cedula.type === 'required') {
+        toast.error('Cedula es un campo requerido');
+      } else if (errors.cedula.type === 'minLength') {
+        toast.error('Cedula requiere una longitud mínima de 10 caracteres');
+      }
+    }
+  }, [errors.cedula]);
 
-    useEffect(() => {
-        if (errors.firstname && errors.firstname.type === 'required') {
-            toast.error('Nombre es un campo requerido');
-        }
-    }, [errors.firstname]);
+  useEffect(() => {
+    if (errors.firstname && errors.firstname.type === 'required') {
+      toast.error('Nombre es un campo requerido');
+    }
+  }, [errors.firstname]);
 
-    useEffect(() => {
-        if (errors.lastname && errors.lastname.type === 'required') {
-            toast.error('Apellido es un campo requerido');
-        }
-    }, [errors.lastname]);
+  useEffect(() => {
+    if (errors.lastname && errors.lastname.type === 'required') {
+      toast.error('Apellido es un campo requerido');
+    }
+  }, [errors.lastname]);
 
-    useEffect(() => {
-        if (errors.issue && errors.issue.type === 'required') {
-            toast.error('Tema es un campo requerido');
-        }
-    }, [errors.issue]);
+  useEffect(() => {
+    if (errors.issue && errors.issue.type === 'required') {
+      toast.error('Tema es un campo requerido');
+    }
+  }, [errors.issue]);
 
-    return (
-        <>
-            <div className='container'>
-                <ToastContainer />
-                <h1>Asignar Estudiante</h1>
-                <form onSubmit={handleSubmit(handleFormSubmit)}>
-                    <div>
-                        <label>Cedula</label>
-                        <input type="text"  {...register('cedula', {
-                            required: true,
-                            minLength: 10,
-                            onChange: handleNumericInputChange
-                        })} />
-                    </div>
-                    <div>
-                        <label>Nombre</label>
-                        <input type="text" {...register('firstname', {
-                            required: true
-                        })} />
-                    </div>
-                    <div>
-                        <label>Apellido</label>
-                        <input type="text" {...register('lastname', {
-                            required: true
-                        })} />
-                    </div>
-                    <div>
-                        <label>Carrera</label>
-                        <select {...register('idCareer')}>
-                            <option value="1">Software</option>
-                            <option value="2">Industrial</option>
-                            <option value="3">Telecomunicaciones</option>
-                            <option value="4">Tegnologías de la Comunicación</option>
-                            <option value="5">Automatización y Robótica</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label>Tema</label>
-                        <input type="text" {...register('issue', {
-                            required: true
-                        })} />
-                    </div>
-                    <div>
-                        <label>Fecha Aprobado</label>
-                        <input type="date" {...register('approvalDate')} />
-                    </div>
-                    <input type="submit" value="Asignar" />
-                </form>
-            </div>
-        </>
-    );
+  return (
+    <div className='container'>
+      <ToastContainer />
+      <h1>Asignar Estudiante</h1>
+      <form onSubmit={handleSubmit(handleFormSubmit)}>
+        <div>
+          <label>Cedula</label>
+          <input type="text"  {...register('cedula', {
+            required: true,
+            minLength: 10,
+            onChange: handleNumericInputChange
+          })} />
+        </div>
+        <div>
+          <label>Nombre</label>
+          <input type="text" {...register('firstname', {
+            required: true
+          })} />
+        </div>
+        <div>
+          <label>Apellido</label>
+          <input type="text" {...register('lastname', {
+            required: true
+          })} />
+        </div>
+        <div>
+          <label>Carrera</label>
+          <select {...register('idCareer')}>
+            <option value="1">Software</option>
+            <option value="2">Industrial</option>
+            <option value="3">Telecomunicaciones</option>
+            <option value="4">Tegnologías de la Comunicación</option>
+            <option value="5">Automatización y Robótica</option>
+          </select>
+        </div>
+        <div>
+          <label>Tema</label>
+          <input type="text" {...register('issue', {
+            required: true
+          })} />
+        </div>
+        <div>
+          <label>Fecha Aprobado</label>
+          <input type="date" {...register('approvalDate')} />
+        </div>
+        <input type="submit" value="Asignar" />
+      </form>
+    </div>
+  );
 };
 
 export default StudentForm;
--- src/home/Menu.tsx
+++ src/home/Menu.tsx
@@ -1,43 +1,70 @@
-import { FloatButton } from "antd";
+import React, { useState } from 'react';
+import { FloatButton, Modal,  } from 'antd';
 import {
   BarsOutlined,
   DisconnectOutlined,
   DiffOutlined,
   UnlockOutlined,
 } from "@ant-design/icons";
+import { request } from '../components/Student/Student.request';
+import StudentForm from '../components/Student/StudentForm';
 
-const Menu: React.FC = () => (
-  <>
-    <FloatButton.Group
-      trigger="hover"
-      type="default"
-      style={{ top: 25, left: 25 }}
-      icon={<BarsOutlined />}
-      shape="square"
-    >
-      <FloatButton
-        tooltip={<div>Añadir Estudiante</div>}
-        icon={<DiffOutlined />}
-        onClick={() => {
-          console.log("Añadiendo estudiante...");
-        }}
-      />
-      <FloatButton
-        tooltip={<div>Cambiar contraseña</div>}
-        icon={<UnlockOutlined />}
-        onClick={() => {
-          console.log("Cambiando contraseña...");
-        }}
-      />
-      <FloatButton
-        tooltip={<div>Cerrar Sesión</div>}
-        icon={<DisconnectOutlined />}
-        onClick={() => {
-          console.log("Cerrando sesión...");
-        }}
-      />
-    </FloatButton.Group>
-  </>
-);
+interface StudentFormProps {
+  TeacherID:number
+}
+
+const Menu: React.FC<StudentFormProps> = ({TeacherID}) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
+
+  const handleFormSubmit = async (data: any) => {
+    await request(data);
+    handleClose();
+  };
+
+
+  return (
+    <>
+      <FloatButton.Group
+        trigger="hover"
+        type="default"
+        style={{ top: 25, left: 25 }}
+        icon={<BarsOutlined />}
+        shape="square"
+      >
+        <FloatButton
+          tooltip={<div>Añadir Estudiante</div>}
+          icon={<DiffOutlined />}
+          onClick={handleShow}
+        />
+        <FloatButton
+          tooltip={<div>Cambiar contraseña</div>}
+          icon={<UnlockOutlined />}
+          onClick={() => {
+            console.log("Cambiando contraseña...");
+          }}
+        />
+        <FloatButton
+          tooltip={<div>Cerrar Sesión</div>}
+          icon={<DisconnectOutlined />}
+          onClick={() => {
+            console.log("Cerrando sesión...");
+          }}
+        />
+      </FloatButton.Group>
+
+      <Modal
+        title="Asignar Estudiante"
+        visible={showModal}
+        onCancel={handleClose}
+        footer={null}
+      >
+        <StudentForm TeacherID = {TeacherID} onSubmit={handleFormSubmit} handleClose={handleClose} />
+      </Modal>
+    </>
+  );
+};
 
 export default Menu;
