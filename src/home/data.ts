38fa20f1820c77d145f614a5c539a3c76b7a67fe
SharpBlade <yepezedwin760@gmail.com>
2024-05-26 20:21:12 -0500

    Cambio en la lógica para obtener datos

--- src/home/data.ts
+++ src/home/data.ts
@@ -2,21 +2,23 @@ import { DataType } from "./types";
 
 let datos: DataType[] = [];
 
-async function fetchData(): Promise<void> {
+async function fetchData(idTeacher: number): Promise<void> {
   try {
-    const response = await fetch("http://localhost:3000/students/");
+    const response = await fetch(
+      `http://localhost:3000/thesis/teacher/${idTeacher}`
+    );
 
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
 
     const responseData = await response.json();
-    datos = responseData.map((item: DataType) => ({
-      id: item.id,
-      name: item.name,
-      career: item.career,
+    datos = responseData.map((item: any) => ({
+      id: item.student.cedula,
+      name: `${item.student.lastname} ${item.student.firstname}`,
+      career: item.student.career.name,
       state: item.state,
-      percentage: item.percentage,
+      percentage: item.finalstate,
     }));
   } catch (error) {
     console.error("Error al obtener los datos:", error);
