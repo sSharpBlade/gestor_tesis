ff59b39064398122b9f846659897b89b6d09f055
SharpBlade <yepezedwin760@gmail.com>
2024-05-26 21:46:44 -0500

    Interfaz del menú oculto

--- src/home/Menu.tsx
+++ src/home/Menu.tsx
@@ -1,12 +1,43 @@
-import IdcardFilled from "@ant-design/icons/lib/icons/IdcardFilled";
-import { Button } from "antd";
+import { FloatButton } from "antd";
+import {
+  BarsOutlined,
+  DisconnectOutlined,
+  DiffOutlined,
+  UnlockOutlined,
+} from "@ant-design/icons";
 
-function Menu() {
-  return (
-    <Button className="">
-      <IdcardFilled />
-    </Button>
-  );
-}
+const Menu: React.FC = () => (
+  <>
+    <FloatButton.Group
+      trigger="hover"
+      type="default"
+      style={{ top: 25, left: 25 }}
+      icon={<BarsOutlined />}
+      shape="square"
+    >
+      <FloatButton
+        tooltip={<div>Añadir Estudiante</div>}
+        icon={<DiffOutlined />}
+        onClick={() => {
+          console.log("Añadiendo estudiante...");
+        }}
+      />
+      <FloatButton
+        tooltip={<div>Cambiar contraseña</div>}
+        icon={<UnlockOutlined />}
+        onClick={() => {
+          console.log("Cambiando contraseña...");
+        }}
+      />
+      <FloatButton
+        tooltip={<div>Cerrar Sesión</div>}
+        icon={<DisconnectOutlined />}
+        onClick={() => {
+          console.log("Cerrando sesión...");
+        }}
+      />
+    </FloatButton.Group>
+  </>
+);
 
 export default Menu;
