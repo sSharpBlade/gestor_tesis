0e0a5a33a6020ba95626dc69acd13c2b6098ed88
TicheKiwar <kiwartiche@gmail.com>
2024-05-27 14:18:56 -0500

    Format to the code

--- src/components/Student/StudentForm.tsx
+++ src/components/Student/StudentForm.tsx
@@ -12,29 +12,29 @@ const StudentForm = () => {
     const onSubmit = async (data: any) => {
         const formattedData = {
             student: {
-              cedula: data.cedula,
-              firstname: data.firstname,
-              lastname: data.lastname,
-              idCareer: parseInt(data.idCareer), // Convertir a número entero
-              idTeacher: 1 // Asignar un valor predeterminado para idTeacher
+                cedula: data.cedula,
+                firstname: data.firstname,
+                lastname: data.lastname,
+                idCareer: parseInt(data.idCareer), // Convertir a número entero
+                idTeacher: 1 // Asignar un valor predeterminado para idTeacher
             },
             thesis: {
-              issue: data.issue,
-              approvalDate: data.approvalDate
+                issue: data.issue,
+                approvalDate: data.approvalDate
             }
-          };
-          request(formattedData);
+        };
+        request(formattedData);
     };
-    
+
 
     const handleNumericInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newValue = e.target.value;
         if (/^\d*$/.test(newValue)) {
             setValue(e.target.name, newValue);
-        }else{
+        } else {
             setValue(e.target.name, newValue.slice(0, -1));
         }
-        if(newValue.length===11){
+        if (newValue.length === 11) {
             setValue(e.target.name, newValue.slice(0, -1));
         }
     };
@@ -66,53 +66,55 @@ const StudentForm = () => {
         }
     }, [errors.issue]);
     return (
-        <div className='container'>
-            <ToastContainer />
-            <h1>Asignar Estudiante</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label >Cedula</label>
-                    <input type="text"  {...register('cedula', {
-                        required: true,
-                        minLength: 10,
-                        onChange: handleNumericInputChange
-                    })} />
-                </div>
-                <div>
-                    <label >Nombre</label>
-                    <input type="text" {...register('firstname', {
-                        required: true
-                    })} />
-                </div>
-                <div>
-                    <label >Apellido</label>
-                    <input type="text" {...register('lastname', {
-                        required: true
-                    })} />
-                </div>
-                <div>
-                    <label >Carrera</label>
-                    <select {...register('idCareer')}>
-                        <option value="1">Software</option>
-                        <option value="2">Industrial</option>
-                        <option value="3">Telecomunicaciones</option>
-                        <option value="4">Tegnologías de la Comunicación</option>
-                        <option value="5">Automatización y Robótica</option>
-                    </select>
-                </div>
-                <div>
-                    <label >Tema</label>
-                    <input type="text" {...register('issue', {
-                        required: true
-                    })} />
-                </div>
-                <div>
-                    <label >Fecha Aprovado</label>
-                    <input type="date" {...register('approvalDate')} />
-                </div>
-                <input type="submit" value="Asignar" />
-            </form>
-        </div>
+        <>
+            <div className='container'>
+                <ToastContainer />
+                <h1>Asignar Estudiante</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label >Cedula</label>
+                        <input type="text"  {...register('cedula', {
+                            required: true,
+                            minLength: 10,
+                            onChange: handleNumericInputChange
+                        })} />
+                    </div>
+                    <div>
+                        <label >Nombre</label>
+                        <input type="text" {...register('firstname', {
+                            required: true
+                        })} />
+                    </div>
+                    <div>
+                        <label >Apellido</label>
+                        <input type="text" {...register('lastname', {
+                            required: true
+                        })} />
+                    </div>
+                    <div>
+                        <label >Carrera</label>
+                        <select {...register('idCareer')}>
+                            <option value="1">Software</option>
+                            <option value="2">Industrial</option>
+                            <option value="3">Telecomunicaciones</option>
+                            <option value="4">Tegnologías de la Comunicación</option>
+                            <option value="5">Automatización y Robótica</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label >Tema</label>
+                        <input type="text" {...register('issue', {
+                            required: true
+                        })} />
+                    </div>
+                    <div>
+                        <label >Fecha Aprovado</label>
+                        <input type="date" {...register('approvalDate')} />
+                    </div>
+                    <input type="submit" value="Asignar" />
+                </form>
+            </div>
+        </>
     );
 };
 
