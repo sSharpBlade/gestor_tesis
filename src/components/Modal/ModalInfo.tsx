12626698f24d3aa8092fedf9e2ea8ee1da9e75a9
SharpBlade <yepezedwin760@gmail.com>
2024-06-06 17:15:59 -0500

    Información del estudiante

--- src/components/Modal/ModalInfo.tsx
+++ src/components/Modal/ModalInfo.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Modal } from "antd";
+import React, { useEffect } from "react";
+import { Modal, Table, TableColumnsType } from "antd";
 
 interface ModalInfoProps {
   isModalOpen: boolean;
@@ -14,17 +14,53 @@ const ModalInfo: React.FC<ModalInfoProps> = ({
   handleCancel,
   student,
 }) => {
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  const columns: TableColumnsType = [
+    {
+      title: "Fecha",
+      width: 100,
+    },
+    {
+      title: "Titulo",
+    },
+    {
+      title: "%",
+      width: 20,
+    },
+    {
+      width: 100,
+    },
+  ];
+
+  useEffect(() => {
+    if (isModalOpen) {
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
+    }
+  }, [isModalOpen]);
+
   return (
     <Modal
+      loading={loading}
+      centered
       title={`Información de ${student?.name || "Estudiante"}`}
       open={isModalOpen}
       onOk={handleOk}
       onCancel={handleCancel}
+      footer={false}
     >
+      <p>Cédula: {student?.id}</p>
       <p>Nombre: {student?.name}</p>
       <p>Carrera: {student?.career}</p>
+      <p>Tema: {student?.issue}</p>
+      <p>Fecha de aprobación: {student?.approvalDate}</p>
       <p>Estado: {student?.state}</p>
       <p>Porcentaje: {student?.percentage}%</p>
+
+      <Table columns={columns}></Table>
     </Modal>
   );
 };
