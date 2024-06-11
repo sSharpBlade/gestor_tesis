3b5941e817d6d9042906205bd8d8b794cf25972b
JaviF <fiallosedisonreggae@gmail.com>
2024-06-11 01:17:05 -0500

    Se agrego la funcionalidad de crear reporte

--- src/components/Report/ReportModal.tsx
+++ src/components/Report/ReportModal.tsx
@@ -6,7 +6,6 @@ interface FormularioReporteProps {
     career: string;
     name: string;
     issue: string;
-    // Añade aquí otras propiedades del estudiante según sea necesario
     id: string;
     approvalDate: string;
     state: string;
@@ -17,62 +16,118 @@ interface FormularioReporteProps {
 
 const FormularioReporte: React.FC<FormularioReporteProps> = ({ student }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const currentDate = new Date().toISOString().slice(0, 10);
+  const [date, setDate] = useState(new Date().toISOString().slice(0, 10));
+  const [reportNumber, setReportNumber] = useState('');
+  const [progressPercentage, setProgressPercentage] = useState<number>(0);
+  const [authorizeSignature, setAuthorizeSignature] = useState(false);
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    // Aquí puedes manejar el envío del formulario
-    console.log('Formulario enviado');
+
+    const informe = {
+      idThesis: student.idThesis,
+      date: date,
+      percentage: progressPercentage,
+      title: reportNumber
+    };
+
+    try {
+      const response = await fetch('http://localhost:3000/reports', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(informe),
+      });
+
+      if (response.ok) {
+        console.log('Informe creado exitosamente');
+        toggleModal(); // Cerrar el modal después de enviar el formulario
+      } else {
+        console.error('Error al crear el informe');
+      }
+    } catch (error) {
+      console.error('Error al enviar la solicitud:', error);
+    }
   };
 
   return (
     <div>
-        <button onClick={toggleModal}>Abrir Formulario</button>
-        {isOpen && (
-            <div className="modal">
-                <div className="modalContent">
-                    <div className="formularioReporte">
-                        <h3>INFORME MENSUAL DEL AVANCE DEL TRABAJO DE TITULACIÓN</h3>
-                        <h3>UNIVERSIDAD TÉCNICA DE AMBATO</h3>
-                        <h3>FACULTAD DE INGENIERÍA EN SISTEMAS ELECTRÓNICA E INDUSTRIAL</h3>
-                        <h3>{student.career.toUpperCase()}</h3><br></br>
-                        <p>
-                            <b>NOMBRE DEL ESTUDIANTE:  </b>{student.name.toUpperCase()}
-                        </p>
-                        <p>
-                            <b>TEMA DEL TRABAJO DE TITULACIÓN:  </b>{student.issue.toUpperCase()}
-                        </p>
-                        <br></br>
-                        <form onSubmit={handleSubmit}>
-                            <p>
-                                <b>FECHA :</b>
-                                <input type='date' name='date' className='dateInput' defaultValue={currentDate} required />
-                            </p>
-                            <p>
-                                <b>TITULO DEL INFORME:</b>
-                                <input type='text' name='reportNumber' className='numInformeInput' required />
-                            </p>
-                            <br></br>
-                            <p>
-                                <b>PORCENTAJE DE AVANCE :</b>
-                                <input type='number' name='progressPercentage' className='percentageInput' min={1} required />
-                            </p>
-                            <p><br></br>
-                                <b>AUTORIZAR FIRMA :</b>
-                                <input type='checkbox' name='authorizeSignature' className='checkboxInput' required />
-                            </p>
-                            <div>
-                                <button type="submit">Guardar</button>
-                            </div>
-                        </form>
-                    </div>
+      <button onClick={toggleModal}>Abrir Formulario</button>
+      {isOpen && (
+        <div className="modal">
+          <div className="modalContent">
+            <div className="formularioReporte">
+              <h3>INFORME MENSUAL DEL AVANCE DEL TRABAJO DE TITULACIÓN</h3>
+              <h3>UNIVERSIDAD TÉCNICA DE AMBATO</h3>
+              <h3>FACULTAD DE INGENIERÍA EN SISTEMAS ELECTRÓNICA E INDUSTRIAL</h3>
+              <h3>{student.career.toUpperCase()}</h3><br></br>
+              <p>
+                <b>NOMBRE DEL ESTUDIANTE:  </b>{student.name.toUpperCase()}
+              </p>
+              <p>
+                <b>TEMA DEL TRABAJO DE TITULACIÓN:  </b>{student.issue.toUpperCase()}
+              </p>
+              <br></br>
+              <form onSubmit={handleSubmit}>
+                <p>
+                  <b>FECHA :</b>
+                  <input
+                    type='date'
+                    name='date'
+                    className='dateInput'
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                    required
+                  />
+                </p>
+                <p>
+                  <b>TITULO DEL INFORME:</b>
+                  <input
+                    type='text'
+                    name='reportNumber'
+                    className='numInformeInput'
+                    value={reportNumber}
+                    onChange={(e) => setReportNumber(e.target.value)}
+                    required
+                  />
+                </p>
+                <br></br>
+                <p>
+                  <b>PORCENTAJE DE AVANCE :</b>
+                  <input
+                    type='number'
+                    name='progressPercentage'
+                    className='percentageInput'
+                    value={progressPercentage}
+                    onChange={(e) => setProgressPercentage(Number(e.target.value))}
+                    min={1}
+                    required
+                  />
+                </p>
+                <p><br></br>
+                  <b>AUTORIZAR FIRMA :</b>
+                  <input
+                    type='checkbox'
+                    name='authorizeSignature'
+                    className='checkboxInput'
+                    checked={authorizeSignature}
+                    onChange={(e) => setAuthorizeSignature(e.target.checked)}
+                    required
+                  />
+                </p>
+                <div>
+                  <button type="submit">Guardar</button>
                 </div>
+              </form>
             </div>
-        )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
