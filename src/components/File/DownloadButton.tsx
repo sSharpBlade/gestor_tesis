c6e7a2d86743deffecf6aff022e969294795a260
SharpBlade <yepezedwin760@gmail.com>
2024-06-23 19:59:08 -0500

    fix download button anexo11

--- src/components/File/DownloadButton.tsx
+++ src/components/File/DownloadButton.tsx
@@ -1,25 +1,50 @@
+import { useState } from "react";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import Anexo11 from "./Anexo11";
 import Button from "antd/es/button";
 
 const DownloadButton = ({ student }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+    console.log(student.idThesis);
+  };
+
   return (
-    <PDFDownloadLink
-      document={<Anexo11 student={student} />}
-      fileName={`${student.issue + " - " + student.name}.pdf`}
-    >
-      {() => (
+    <div>
+      {isClicked ? (
+        <PDFDownloadLink
+          document={<Anexo11 student={student} />}
+          fileName={`${student.issue + " - " + student.name}.pdf`}
+        >
+          {({ loading }) =>
+            loading ? (
+              "Generando PDF..."
+            ) : (
+              <Button
+                className="border-0 bg-transparent mt-3"
+                onClick={() =>
+                  setTimeout(() => {
+                    setIsClicked(false);
+                  }, 500)
+                }
+              >
+                Descargar PDF
+              </Button>
+            )
+          }
+        </PDFDownloadLink>
+      ) : (
         <Button
-          //disabled={student.percentage !== 100}
-          className="border-0 bg-transparent"
-          onClick={() => {
-            console.log(student.idThesis);
-          }}
+          // disabled={student.percentage !== 100}
+          className="border-0 bg-transparent mt-3"
+          onClick={handleClick}
         >
           Generar Anexo11
         </Button>
       )}
-    </PDFDownloadLink>
+    </div>
   );
 };
 
