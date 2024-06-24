4e6c5f57ad42523d0cc98abe5572c11143c25f75
kiwar <kiwartiche@gmail.com>
2024-06-24 15:26:34 -0500

    modificacion Generar PDF

--- src/components/FileAnexo5/DownloadButton5.tsx
+++ src/components/FileAnexo5/DownloadButton5.tsx
@@ -1,21 +1,23 @@
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import Button from "antd/es/button";
 import Anexo5 from "./Anexo5";
+import { notification } from "antd";
 
 const DownloadButtonAnexo5 = ({ student, idReport,refreshKey }) => {
+  const showDownloadNotification = () => {
+    notification.success({
+      message: 'Descarga Iniciada',
+      description: 'La descarga del Anexo 5 ha comenzado.',
+    });
+  };
   return (
     <PDFDownloadLink
       document={<Anexo5 student={student} idReport={idReport} refreshKey={refreshKey}/>}
       fileName={`${student.issue + " - " + student.name+ " - Anexo 5" }.pdf`}
     >
-      {() => (
-        <Button
-          className="border-0 bg-transparent"
-          onClick={() => {
-            console.log(student.idThesis);
-          }}
-        >
-          Generar Anexo 5
+      {({ loading }) => (
+        <Button className="border-0 bg-transparent" onClick={showDownloadNotification}>
+          {loading ? "Generando..." : "Generar Anexo 5"}
         </Button>
       )}
     </PDFDownloadLink>
